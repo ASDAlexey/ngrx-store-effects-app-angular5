60a0b7297808f9824c3a06f7d35103f8f3c946f7
Alexey <asdalexey@yandex.ru>
2018-06-18 23:21:46 +0300

    #15 add reducers

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { reducers } from './store';
+
 // components
 import * as fromComponents from './components';
 
@@ -12,21 +14,13 @@ import * as fromContainers from './containers';
 
 // services
 import * as fromServices from './services';
+import { StoreModule } from '@ngrx/store';
 
 // routes
 export const ROUTES: Routes = [
-  {
-    path: '',
-    component: fromContainers.ProductsComponent,
-  },
-  {
-    path: ':id',
-    component: fromContainers.ProductItemComponent,
-  },
-  {
-    path: 'new',
-    component: fromContainers.ProductItemComponent,
-  },
+  { path: '', component: fromContainers.ProductsComponent },
+  { path: ':id', component: fromContainers.ProductItemComponent },
+  { path: 'new', component: fromContainers.ProductItemComponent },
 ];
 
 @NgModule({
@@ -35,9 +29,11 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
+    StoreModule.forFeature('products', reducers)
   ],
   providers: [...fromServices.services],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })
-export class ProductsModule {}
+export class ProductsModule {
+}
