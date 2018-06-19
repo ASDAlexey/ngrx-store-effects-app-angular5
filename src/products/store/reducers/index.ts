c18d04ef427bc02aa96d23d0efbdc6c12bff7025
Alexey <asdalexey@yandex.ru>
2018-06-20 00:01:14 +0300

    #16 add selectors

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,6 +1,5 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromPizzas from './pizzas.reducer';
-import { PizzasState } from './pizzas.reducer';
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzasState,
@@ -9,3 +8,14 @@ export interface ProductsState {
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: fromPizzas.reducer,
 };
+
+export const getProductsState = createFeatureSelector<ProductsState>('products');
+
+export const getPizzaState = createSelector(
+  getProductsState,
+  (state: ProductsState) => state.pizzas
+);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
